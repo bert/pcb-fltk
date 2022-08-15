c6a96470f74d9d247164ef95f8be432968991979
bert <bert.timmerman@xs4all.nl>
2022-08-16 01:19:01 +0200

    Added a file chooser to the save one connection callback.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/hid/fltk/main_window.cpp
+++ src/hid/fltk/main_window.cpp
@@ -1116,7 +1116,35 @@ Main_Window::load_vendor_resource_file_CB (Fl_Widget *, Main_Window *mw)
 void
 Main_Window::save_conn_data_1_CB (Fl_Widget *, Main_Window *mw)
 {
-  /*! \todo Add code here. */
+  Fl_Native_File_Chooser fnfc;
+  fnfc.title ("Save a single net as ...");
+  fnfc.type (Fl_Native_File_Chooser::BROWSE_SAVE_FILE);
+  fnfc.options (Fl_Native_File_Chooser::SAVEAS_CONFIRM | Fl_Native_File_Chooser::NEW_FOLDER | Fl_Native_File_Chooser::PREVIEW);
+  fnfc.filter ("*.net"); /* Add a filter for netlist suffixes. */
+  fnfc.directory ("."); /* The default directory to use. */
+  /* Show native file chooser. */
+  switch (fnfc.show ())
+  {
+    case -1: /* An error occured. */
+      fprintf (stderr, "ERROR: %s\n", fnfc.errmsg ());
+      break;
+
+    case 1: /* The [CANCEL] button was clicked, or the dialog was closed by clickin the top right [x]. */
+#if DEBUG
+      fprintf (stderr, "The file chooser was cancelled.\n");
+#endif
+      break;
+
+    default: /* A file was chosen. */
+#if DEBUG
+      fprintf (stderr, "The file chooser picked: %s\n", fnfc.filename ());
+#endif
+  /*! \todo Add code to:
+   * - Return the chosen filename to the pcb engine.
+   * - Clean up the obsolete file chooser object.
+   */
+      break;
+  }
 }
 
 
